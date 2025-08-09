b731507e45b9de945fed7a54ab8e3496cf17d566
tejing <tejing163@msn.com>
2025-08-09 14:15:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # 修改openwrt登陆地址,把下面的 192.168.10.1 修改成你想要的就可以了
-sed -i 's/192.168.100.1/192.168.50.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.100/192.168.50/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
 # sed -i 's/OpenWrt/iStore OS/g' package/base-files/files/bin/config_generate
@@ -25,8 +25,25 @@ sed -i 's/192.168.100.1/192.168.50.1/g' package/base-files/files/bin/config_gene
 # CONFIG_PACKAGE_luci-app-adguardhome=y
 # CONFIG_PACKAGE_luci-app-openclash=y
 # ' >> .config
+status=$(curl -H "Authorization: token $GITHUB_TOKEN" -s "https://api.github.com/repos/kiddin9/kwrt-packages/actions/runs" | jq -r '.workflow_runs[0].status')
+echo "$status"
+while [[ "$status" == "in_progress" || "$status" == "queued" ]];do
+	echo "wait 5s"
+	sleep 5
+	status=$(curl -H "Authorization: token $GITHUB_TOKEN" -s "https://api.github.com/repos/kiddin9/kwrt-packages/actions/runs" | jq -r '.workflow_runs[0].status')
+done
 
-echo '
-CONFIG_PACKAGE_luci-app-pppoe-relay=y
-CONFIG_PACKAGE_rp-pppoe-relay=y' >> .config
+mv -f feeds/kiddin9/r81* tmp/
+
+sed -i \
+	-e "s/+\(luci\|luci-ssl\|uhttpd\)\( \|$\)/\2/" \
+	-e "s/+nginx\( \|$\)/+nginx-ssl\1/" \
+	-e 's/+python\( \|$\)/+python3/' \
+	-e 's?../../lang?$(TOPDIR)/feeds/packages/lang?' \
+	package/feeds/kiddin9/*/Makefile
+
+mv -f tmp/r81* feeds/kiddin9/
+
+echo 'CONFIG_PACKAGE_luci-app-mosdns=y
+CONFIG_PACKAGE_luci-app-pppoe-relay=y' >> .config
 
